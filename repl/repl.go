a55ce36fa0d326a12f41f2500a5638c347930a11
emperWang <544094478@qq.com>
2024-09-21 15:16:55 +0800

    repl

--- repl/repl.go
+++ repl/repl.go
@@ -3,10 +3,11 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 
 	"com.lanuage/monkey/lexer"
-	"com.lanuage/monkey/token"
+	"com.lanuage/monkey/parser"
 )
 
 func Repl() {
@@ -25,9 +26,21 @@ func Repl() {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(os.Stdout, "%+v\n", tok)
+		if len(p.Errors()) > 0 {
+			printParseErrors(os.Stderr, p.Errors())
 		}
+
+		io.WriteString(os.Stdout, p.ParserProgram().String())
+		io.WriteString(os.Stdout, "\n")
+
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+
+	for _, err := range errors {
+		io.WriteString(out, err+"\n")
 	}
 }
